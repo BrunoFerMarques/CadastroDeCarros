102af9f0776ae4676b26012b38620bd9feefa57b
BrunoFerMarques <brunofernandesmarques259@gmail.com>
2024-10-23 14:29:09 -0300

    terminando form para pagina create

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head/>
-      <header className="flex justify-between itens-center bg-red-600 h-full bg-red-600">
+      <header className="static flex justify-between itens-center bg-red-600 h-full bg-red-600">
         <a href="/" className="p-5"> <Image width = {100}src = {Carlogo} alt="Logo da empresa" /></a>
         <div className="p-10"><Menu/></div>
       </header>
--- pages/create.tsx
+++ pages/create.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import next from "next"
+import mysql from 'mysql2'
+import Document from "next/document"
+
+
 
 const create = () => {
   return (
-    <div className=''>
-        <a href='/'>Pagina inicial</a>
-        <a href='/Update'>Update</a>
-        <a href='/Delete'>Delete</a>
-    </div>
+  <div className="bg-black min-h-screen flex items-center justify-center">
+    <form className="grid grid-cols-2 gap-4 bg-gray-400 border-2 p-5">
+      <div>
+        <label className="block p-5">Marca do carro:</label>
+        <input className="block w-full mb-4 p-2" placeholder="Digite a marca do carro aqui" type='name' id="brand"/>
+        <label className="block p-5">Modelo do carro:</label>
+        <input className="block w-full mb-4 p-2" placeholder="Modelo do carro" type='name' id="model" />
+      </div>
+      <div>
+        <label className="block p-5">Placa do carro:</label>
+        <input className="block w-full mb-4 p-2" placeholder="Placa do carro" id="plate"/>
+        <label className="block p-5">Valor do carro:</label>
+        <input className="block w-full mb-4 p-2" placeholder="Valor do carro" id="price"/>
+      </div>
+      <div className="col-span-2 flex justify-center">
+        <button type="submit" className="bg-black text-white mt-5 px-4 py-2">Cadastrar</button>
+      </div>
+    </form>
+  </div>
+
   )
 }
- 
+
+
+
 export default create
